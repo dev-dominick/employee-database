5666ca11a44814a95363f754345ce2540724bd41
Dom <Domalbano35@gmail.com>
2022-04-17 15:15:27 -0400

    set up opening inquirer prompt and made folder and file for sql db

--- index.js
+++ index.js
@@ -1,116 +1,145 @@
-const fs = require("fs");
 const inquirer = require("inquirer");
 const express = require("express");
 const cTable = require("console.table");
 const mysql = require("mysql2");
 
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "password1!",
+    database: "employee_db",
+  },
+  console.log(`Connected to employee database.`)
+);
 
-
-
-const questions = [
-  {
-    type: "list",
-    message: "What would you like to do?",
-    choices: ["View All Employees", "Add Employee", "Update Employee Role", "View All Role", "View All Departments", "Add Department", "Quit", "View All Employees"],
-    name: "openingPrompt",
-  },
-  {
-    type: "input",
-    message: "What is your name?",
-    name: "name",
-  },
-  {
-    type: "input",
-    message: "What is your employee ID?",
-    name: "iD",
-  },
-  {
-    type: "input",
-    message: "What is your email address?",
-    name: "email",
-  },
-  {
-    type: "input",
-    message: "What is your office number?",
-    name: "officeNumber",
-    when: (answers) => {
-      if (answers.employeeTitle === "Manager") {
-        return true;
-      }
-    },
-  },
-  {
-    type: "input",
-    message: "What is your GitHub username?",
-    name: "gitHub",
-    when: (answers) => {
-      if (answers.employeeTitle === "Engineer") {
-        return true;
-      }
-    },
-  },
-  {
-    type: "input",
-    message: "What school are you attending?",
-    name: "school",
-    when: (answers) => {
-      if (answers.employeeTitle === "Intern") {
-        return true;
-      }
-    },
-  },
-  {
-    type: "list",
-    message: "Would you like to add another employee?",
-    choices: ["Yes", "No"],
-    name: "option",
-  },
-];
+//opening prompt 
+const openingPrompt = [
+  
+    {
+      type: "list",
+      name: "firstPrompt",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "Add Employee",
+        "Update Employee Role",
+        "View All Role",
+        "View All Departments",
+        "Add Department",
+        "Quit",],
+    }
+  ];
 
 function init() {
-  inquirer.prompt(questions).then((answers) => {
-    switch (answers.positionTitle) {
-      case "Manager":
-        employeeArray = new Manager(
-          answers.name,
-          answers.iD,
-          answers.email,
-          answers.officeNumber
-        );
-        break;
-      case "Engineer":
-        employeeArray = new Engineer(
-          answers.name,
-          answers.iD,
-          answers.email,
-          answers.gitHub
-        );
-        break;
-      case "Intern":
-        employeeArray = new Intern(
-          answers.name,
-          answers.iD,
-          answers.email,
-          answers.School
-        );
-        break;
-      default:
-        break;
-    }
+  inquirer.prompt(openingPrompt).then((answers) => {
+      switch (choices) {
+    case "View All Employees":
+      showAllEmployees();
+      break;
 
-    if (answers.option == "Yes") {
-      init();
-    }
-    // fix whatever is making this make new html file
-    fs.writeFileSync("index.html", teamBuilder(answers), (err) => {
-      if (err) {
-        console.log(err);
-        return;
-      } else {
-        console.log("Success!");
-      }
-    });
-  });
-}
+    case "Add Employee":
+      addEmployee();
+      break;
+
+    case "Update Employee Role":
+      updateEmployeeRole();
+      break;
+
+    case "View All Role":
+      viewAllRoles();
+      break;
+
+    case "View All Departments":
+      ViewAllDepartments();
+      break;
+
+    case "Add Department":
+      addDepartment();
+      break;
+
+    case "Quit":
+      quit();
+      break;
+
+    default:
+      break;
+
+
+
+  }
+  })
+};
 
 init();
+
+
+
+//     // this will control what happens depending on what choice is selected, 
+//     .then(answers) => {
+//   const { choices } = answers;
+
+//   switch (choices) {
+//     case "View All Employees":
+//       showAllEmployees();
+//       break;
+
+//     case "Add Employee":
+//       addEmployee();
+//       break;
+
+//     case "Update Employee Role":
+//       updateEmployeeRole();
+//       break;
+
+//     case "View All Role":
+//       viewAllRoles();
+//       break;
+
+//     case "View All Departments":
+//       ViewAllDepartments();
+//       break;
+
+//     case "Add Department":
+//       addDepartment();
+//       break;
+
+//     case "Quit":
+//       quit();
+//       break;
+
+//     default:
+//       break;
+
+
+
+//   }
+// }
+
+
+
+
+// GIVEN a command-line application that accepts user input
+// WHEN I start the application
+// THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+
+// WHEN I choose to view all departments
+// THEN I am presented with a formatted table showing department names and department ids
+
+// WHEN I choose to view all roles
+// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+
+// WHEN I choose to view all employees
+// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+
+// WHEN I choose to add a department
+// THEN I am prompted to enter the name of the department and that department is added to the database
+
+// WHEN I choose to add a role
+// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+
+// WHEN I choose to add an employee
+// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+
+// WHEN I choose to update an employee role
+// THEN I am prompted to select an employee to update and their new role and this information is updated in the database
